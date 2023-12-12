b2d143362e4ac4535da84858db1650309ed4081d
Joel Marano <joelmarano1@gmail.com>
2023-12-12 23:33:36 +0800

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,11 +8,11 @@ const Hero = () => {
                 <div className="lg:col-span-6">
                 </div>
                 <div className="lg:col-span-6 md:px-4 text-center lg:text-left">
-                    <p className="text-7xl font-extrabold  pl-7">
+                    <p className="text-7xl font-extrabold  lg:pl-7">
                         We care for your furry little loved ones while
                     </p> 
                     <div className="py-10 text-center lg:text-left">
-                        <Link to="_appointment" smooth={true}  duration={500} className="bg-white px-10 py-3 rounded-full ml-8  text-black hover:bg-gray-300"> Schedule a visit </Link>
+                        <Link to="_appointment" smooth={true}  duration={500} className="bg-white px-10 py-3 rounded-full lg:ml-8  text-black hover:bg-gray-300"> Schedule a visit </Link>
                     </div>
                 
                 </div>
